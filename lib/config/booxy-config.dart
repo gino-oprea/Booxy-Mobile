f2171353672be81ed1cdaa1f374570d2bc494cc3
Gino <gino.oprea24@gmail.com>
2020-11-19 22:57:08 +0200

    beta release setup

--- lib/config/booxy-config.dart
+++ lib/config/booxy-config.dart
@@ -1,7 +1,7 @@
 class BooxyConfig {
-  static const String api_endpoint = 'https://apidemo.booxy.ro/api/';
+  static const String api_endpoint = 'https://api.booxy.ro/api/';
   static const String token_api_endpoint =
-      'https://identitydemo.booxy.ro/connect/token';
+      'https://identity.booxy.ro/connect/token';
   static const String GOOGLE_MAPS_API_KEY =
       'AIzaSyC8w59rnBMxP6yWIKKOMpEFOdlAbKh5_Y8';
 }
